a55e8fce79df9d2825efafc6cbfb333a1d64d9ec
Aleksandr <ak@symflex.ru>
2019-03-08 11:00:50 +0500

    save

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     Sonata\BlockBundle\SonataBlockBundle::class => ['all' => true],
     Knp\Bundle\MenuBundle\KnpMenuBundle::class => ['all' => true],
     Sonata\AdminBundle\SonataAdminBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
